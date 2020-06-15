8be153bf5ff824915038049ca201534724451b77
Denis Utin <denis.u.workk@gmail.com>
2020-06-15 20:26:18 +0400

    search_uniq_elements_with_list

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -2,35 +2,31 @@ package com.company;
 
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Scanner;
+import java.util.List;
+import java.io.File;
 
 public class Main {
 
     /**
-     * Get count of unique addresses in file, on unix systems.
+     * Get count of unique addresses in file.
      *
      * @param args
      * @throws IOException
      * @throws InterruptedException
      */
-    public static void main(String[] args) throws InterruptedException, IOException {
-        messageOfCount();
-        new ProcessBuilder(argumentsForProcessBuilder()).inheritIO().start().waitFor();
-    }
-
-    /**
-     * Executes the file with sh (Bourne shell).
-     * -u  delete duplicate entries in sorting results
-     * Result is redirected to the wc utility, which simply counts the number of lines.
-     */
-    private static ArrayList<String> argumentsForProcessBuilder() {
-        ArrayList<String> arguments = new ArrayList<>();
-        arguments.add("/bin/sh");
-        arguments.add("-c");
-        arguments.add("sort -u data.txt | wc -l");
-        return arguments;
-    }
-
-    private static void messageOfCount() {
-        System.out.println("Count of unique addresses in file: ");
+    public static void main(String[] args) throws IOException {
+        Scanner sc = new Scanner(new File("data.txt"));
+        List<String> lines = new ArrayList<String>();
+        while (sc.hasNextLine()) {
+            lines.add(sc.nextLine());
+        }
+        List<String> myList = new ArrayList<>();
+        for (String x : lines) {
+            if (!myList.contains(x))
+                myList.add(x);
+        }
+        System.out.println("Array - " + lines);
+        System.out.println("Count of unique elements - " + myList.size());
     }
 }
